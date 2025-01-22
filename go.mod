c838e1601d0b649cd0024f6997b315bc7abd94dc
marcos802313 <802.313@alunos.unigran.br>
2025-01-22 08:59:00 -0400

    Primeiro commit

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module api-go
 
 go 1.23
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require github.com/go-chi/chi/v5 v5.2.0
--- m.go
+++ m.go
@@ -1,8 +1,37 @@
 package main
 
-import "github.com/go-chi/chi/v5"
+import (
+	"api-go/api"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
-	r := chi.NewMux()
-	_ = r
+	if err := run(); err != nil {
+		slog.Error("failed to execute code", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("all systems offline")
+}
+
+func run() error {
+	db := make(map[string]string)
+	handler := api.NewHandler(db)
+
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
 }
